05722cb153e1385ae4c89b55b95eeaa65c7baeb0
Jorgen Cani <jgen.cani@gmail.com>
2024-04-17 13:46:56 +0300

    bug: return 3 events, not all

--- api/recommendations/random.py
+++ api/recommendations/random.py
@@ -1,10 +1,9 @@
 from .utils import get_all_events
+from random import sample
 
 
 def random_recommendation():
     num_of_recommendations = 3
     all_events = get_all_events()
-    recommendations = [
-        event for event in all_events for _ in range(num_of_recommendations)
-    ]
+    recommendations = sample(all_events, num_of_recommendations)
     return recommendations
