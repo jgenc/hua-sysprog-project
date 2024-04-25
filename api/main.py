c91a554c26a01c676d069493072a73e07f5b76ed
Jorgen Cani <jgen.cani@gmail.com>
2024-04-25 13:08:31 +0300

    include routes into main FastAPI object

--- api/main.py
+++ api/main.py
@@ -1,21 +1,18 @@
-from typing import Union
-from datetime import datetime
-import random
 import logging
 
-from fastapi import FastAPI, HTTPException
-import pandas as pd
+from fastapi import FastAPI
 
-from .routers import users
+from .routers import users, events, coupons, recommendations
 from .data.betting import BettingData
-from .schemas import Coupon, Event, User, Selection, Recommendations, NewUser
-from .recommendations import most_bet_sport_recommenedation
 
 logger = logging.getLogger("api")
 # logger.disabled = True
 
 app = FastAPI()
-app.include_router(users.router, prefix="/user")
+app.include_router(users.router)
+app.include_router(events.router)
+app.include_router(coupons.router)
+app.include_router(recommendations.router)
 
 df = BettingData("./api/data/dummy.json")
 
@@ -23,53 +20,6 @@ df = BettingData("./api/data/dummy.json")
 @app.get("/")
 def read_root():
     return {"msg": "Hello World"}
-
-
-@app.get("/event/random", response_model=Event)
-def get_event_random() -> Event:
-    return Event(**df.events.sample(1).to_dict(orient="records")[0])
-
-
-@app.get("/event/{event_id}", response_model=Event)
-def get_event(event_id: str) -> Event:
-    return Event(
-        **df.events[df.events["event_id"] == event_id].to_dict(orient="records")[0]
-    )
-
-
-@app.get("/coupon/random", response_model=Coupon)
-def get_coupon_random() -> Coupon:
-    return Coupon(**df.coupons.sample(1).to_dict(orient="records")[0])
-
-
-@app.get("/coupon/{coupon_id}", response_model=Coupon)
-def get_coupon(coupon_id: int) -> Coupon:
-    try:
-        coupon = df.coupons[df.coupons["coupon_id"] == coupon_id].to_dict(
-            orient="records"
-        )[0]
-    except IndexError:
-        raise HTTPException(status_code=404, detail="Coupon not found")
-
-    return Coupon(**coupon)
-
-
-@app.get("/coupons/{user_id}", response_model=list[Coupon])
-def get_coupon_userid(user_id: int) -> list[Coupon]:
-    user_id_exists = df.users["user_id"].isin([user_id]).any()
-    if not user_id_exists:
-        raise HTTPException(status_code=404, detail="User not found")
-
-    return [
-        Coupon(**x)
-        for x in df.coupons[df.coupons["user_id"] == user_id].to_dict(orient="records")
-    ]
-
-
-@app.get("/recommendation/{user_id}", response_model=Recommendations)
-def get_recommendation(user_id: int) -> Recommendations:
-    recommended_events = most_bet_sport_recommenedation(user_id)
-    return Recommendations(events=recommended_events)
 
 
 if __name__ == "__main__":
