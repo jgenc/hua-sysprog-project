146d87db7afab40bcf2d0ac3db3e664e8fbb1218
Jorgen Cani <jgen.cani@gmail.com>
2024-07-02 17:56:33 +0300

    timing metrics and environment variables

--- api/data/upload_dummy.py
+++ api/data/upload_dummy.py
@@ -1,5 +1,11 @@
-import httpx
+import os
 import json
+import httpx
+
+from api.tools.timing_functions import timeit
+
+# Default port is 8098 for the docker-compose environment
+PORT = os.environ.get("PORT", 8098)
 
 with open("./api/data/dummy.json", "r") as f:
     js = json.load(f)
@@ -8,46 +14,55 @@ users = js["users"]
 events = js["events"]
 coupons = js["coupons"]
 
-for user in users:
-    r = httpx.post(
-        "http://0.0.0.0:8098/user",
-        json={
-            "birth_year": user["birth_year"],
-            "country": user["country"],
-            "currency": user["currency"],
-            "gender": user["gender"],
-        },
-    )
-    if r.status_code != 200:
-        print(f"Error with {user["id"]}")
+@timeit
+def create_users():
+    for user in users:
+        r = httpx.post(
+            f"http://localhost:{PORT}/users",
+            json={
+                "birth_year": user["birth_year"],
+                "country": user["country"],
+                "currency": user["currency"],
+                "gender": user["gender"]
+            },
+        )
+        if r.status_code != 200:
+            print(f"Error with {user["id"]=}")
 
-for event in events:
-    r = httpx.post(
-        "http://0.0.0.0:8098/event",
-        json={
-            "begin_timestamp": event["begin_timestamp"],
-            "end_timestamp": event["end_timestamp"],
-            "country": event["country"],
-            "league": event["league"],
-            "sport": event["sport"],
-            "participants": [
-                event["participants"][0],
-                event["participants"][1]
-            ]
-        },
-    )
-    if r.status_code != 200:
-        print(f"Error with {event["id"]}")
+@timeit
+def create_events():
+    for event in events:
+        r = httpx.post(
+            f"http://localhost:{PORT}/events",
+            json={
+                "begin_timestamp": event["begin_timestamp"],
+                "end_timestamp": event["end_timestamp"],
+                "country": event["country"],
+                "league": event["league"],
+                "sport": event["sport"],
+                "participants": [
+                    event["participants"][0],
+                    event["participants"][1]
+                ]
+            },
+        )
+        if r.status_code != 200:
+            print(f"Error with {event["id"]=}")
 
+@timeit
+def create_coupons():
+    for coupon in coupons:
+        r = httpx.post(
+            f"http://localhost:{PORT}/coupons",
+            json={
+                "user_id": coupon["user_id"],
+                "stake": coupon["stake"],
+                "selections": [selection for selection in coupon["selections"]]
+            },
+        )
+        if r.status_code != 200:
+            print(f"Error with {coupon["id"]=}")
 
-for coupon in coupons:
-    r = httpx.post(
-        "http://0.0.0.0:8098/coupon",
-        json={
-            "user_id": coupon["user_id"],
-            "stake": coupon["stake"],
-            "selections": [selection for selection in coupon["selections"]]
-        },
-    )
-    if r.status_code != 200:
-        print(f"Error with {user["id"]}")
+create_users()
+create_events()
+create_coupons()
--- api/dependencies/database.py
+++ api/dependencies/database.py
@@ -1,8 +1,15 @@
 from sqlmodel import SQLModel, create_engine, Session
 from api.models import user
+import os
 
-sqlite_file_name = "user_data.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
+# Local development will use the relative path
+if os.getenv("ENV") == "dev":
+    sqlite_file_name = "user_data_dev.db"
+    sqlite_url = f"sqlite:///{sqlite_file_name}"
+else:
+    sqlite_file_name = "user_data.db"
+    # sqlite db will live in /user_data.db in the container
+    sqlite_url = f"sqlite:////api/data/{sqlite_file_name}"
 
 engine = create_engine(sqlite_url)
 
